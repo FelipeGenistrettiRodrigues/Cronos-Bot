26a03cc3e31b531b7c7a75cd98fb2bc3560dd2b3
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2026-07-12 22:51:40 -0300

    chore: remove pastas bin e obj do rastreamento

--- Application.CronosBot/obj/Debug/net10.0/Application.CronosBot.AssemblyInfo.cs
+++ Application.CronosBot/obj/Debug/net10.0/Application.CronosBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application.CronosBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be0eb05f731c3a38bad298eb2a1a938eef90aab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b53ada347d0b0caf262d9098e9a3e1d9662de1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application.CronosBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application.CronosBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
